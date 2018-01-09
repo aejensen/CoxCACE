c0cb5990c5949bd3832657f5c5b421e476be3410
Andreas Jensen <aeje@sund.ku.dk>
2018-01-09 03:18:44 +0100

    .

--- R/classFunctions.R
+++ R/classFunctions.R
@@ -1,5 +1,5 @@
 print.CoxCACE <- function(x, ...) {
-  print("CACE = ", x$CACE, "\n")
+  print(paste("CACE = ", signif(x$CACE,4)))
   invisible(x)
 }
 
@@ -8,3 +8,19 @@ coef.CoxCACE <- function(x, ...) {
   x$CACE
 }
 
+summary.CoxCACE <- function(object, ...) {
+  cat("Complier Average Causal Effect Cox Proportional Hazards\n\n")
+
+  cMat <- matrix(NA, 1, 4)
+  cMat[1,1] <- object$CACE
+  cMat[1,2] <- NA
+  cMat[2,3] <- NA
+  cMat[2,4] <- NA
+  colnames(cMat) <- c("Estimate", "Std. Error", "z value", "P(>|z|)")
+  rownames(cMat) <- c("CACE")
+
+  cat("Coefficients:\n")
+  print.default(cMat)
+
+  invisible(object)
+}
