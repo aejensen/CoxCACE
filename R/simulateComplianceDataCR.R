e8371f501b619690b9b74ea6d2f4b36535d17b0d
Andreas Jensen <aeje@sund.ku.dk>
2018-01-21 22:02:27 +0100

    fixing simulation code

--- R/simulateComplianceDataCR.R
+++ R/simulateComplianceDataCR.R
@@ -1,6 +1,7 @@
 simulateComplianceDataCR <- function(n, psi1, psi2) {
   p.tr <- 0.5  # Probability of assigned to treatment
   pc.tr <- 0.6 # Probability of compliance
+
   lambda00.1 <- 1 / 40 #non-complier baseline hazard for cause 1
   lambda01.1 <- 1 / 80 #complier baseline hazard for cause 1
 
@@ -13,18 +14,19 @@ simulateComplianceDataCR <- function(n, psi1, psi2) {
   #Simulate compliance
   E1 <- rbinom(n, 1, pc.tr)
 
+  #Number of subjects in each principal stratum
   n00 <- sum((R == 0) & (E1 == 0))
   n01 <- sum((R == 0) & (E1 == 1))
   n10 <- sum((R == 1) & (E1 == 0))
   n11 <- sum((R == 1) & (E1 == 1))
 
-  #Simulate event times
+  #Simulate event times for each principal stratum
   T00.tmp0 <- rexp(n00) / (lambda00.1 + lambda00.2)
   T01.tmp0 <- rexp(n01) / (lambda01.1 + lambda01.2)
   T10.tmp0 <- rexp(n10) / (lambda00.1 + lambda00.2)
   T11.tmp0 <- rexp(n11) / (lambda01.1 * exp(psi1) + lambda01.2 * exp(psi2))
 
-  #Simulate event type
+  #Simulate the event type
   cause00 <- sample(1:2, n00, replace=TRUE,
                     prob = c(lambda00.1 / (lambda00.1 + lambda00.2),
                              lambda00.2 / (lambda00.1 + lambda00.2)))
@@ -40,7 +42,8 @@ simulateComplianceDataCR <- function(n, psi1, psi2) {
   cause11 <- sample(1:2, n11, replace=TRUE,
                     prob = c((lambda01.1 * exp(psi1)) / (lambda01.1 * exp(psi1) + lambda01.2 * exp(psi2)),
                              (lambda01.2 * exp(psi2)) / (lambda01.1 * exp(psi1) + lambda01.2 * exp(psi2))))
-  #Simulate censoring times
+
+  #Simulate thecensoring times
   cen1 <- 60
   cen2 <- 100
   entryt2 <- 75
@@ -72,6 +75,7 @@ simulateComplianceDataCR <- function(n, psi1, psi2) {
   status11 <- 0 * T11
   status11[T11 == T11.tmp0] <- cause11[T11 == T11.tmp0]
 
+  #Combine in the form of the observed data
   d10 <- cbind(T10, status10, E1[(R == 1) & (E1 == 0)], R[(R == 1) & (E1 == 0)])
   d11 <- cbind(T11, status11, E1[(R == 1) & (E1 == 1)], R[(R == 1) & (E1 == 1)])
   d0 <- cbind(c(T00, T01), c(status00, status01), rep(0, n00 + n01), rep(0, n00 + n01))
